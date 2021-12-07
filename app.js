ef10805275fafb53c6e5237e0e76aacaeb00bb87
s2ljeun <s2ljeun@gmail.com>
2021-12-07 14:59:20 +0900

    Css in JavaScript

--- app.js
+++ app.js
@@ -1,38 +1,16 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
+// function handleTitleClick(){
+//   const clickedClass = "clicked";
+//   if(h1.classList.contains(clickedClass)){
+//     h1.classList.remove(clickedClass);
+//   } else {
+//     h1.classList.add(clickedClass);
+//   }
+// }
+
 function handleTitleClick(){
-  h1.style.color = "blue";
-}
-
-function handleMouseEnter(){
-  h1.innerText = "Mouse is here!";
-}
-
-function handleMouseleave(){
-  h1.innerText = "Mouse is gone!";
-}
-
-function handleWindowResize(){
-  document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-  alert("copier!");
-}
-
-function handleWindowOffline(){
-  alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-  alert("All Gooood");
+  h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseleave);
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
